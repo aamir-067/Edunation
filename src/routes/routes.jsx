0fdbe352e21f05a02f118abc6430f34cccec5494
aamir <aamirkhan@engineer.com>
2024-06-03 21:12:20 +0500

    feat(src): added react-router

---
+++ src/routes/routes.jsx
@@ -0,0 +1,18 @@
+import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+import { Hero } from "../components/Hero";
+import { Donate } from "../components/Donate";
+import { Withdraw } from "../components/Withdraw";
+import { AboutUs } from "../components/AboutUs";
+import { Error } from "../components/Error"
+import App from "../App";
+
+export const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<App />}>
+        <Route path="" element={<Hero />} />
+        <Route path="donate" element={<Donate />} />
+        <Route path="withdraw" element={<Withdraw />} />
+        <Route path="about" element={<AboutUs />} />
+        <Route path="*" element={<Error />} />
+
+    </Route>
+))
