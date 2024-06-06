f52cec346472d2fc7c0ca2f6f6c8c4b87c0d2de0
aamir <aamirkhan@engineer.com>
2024-06-06 17:12:46 +0500

    fix: change charity owner

--- hh/hardhat.config.js
+++ hh/hardhat.config.js
@@ -1,8 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
 const rpcUrl = "https://eth-sepolia.g.alchemy.com/v2/Wy3FsIl1RBSQqgXDNz-eq-XDzOaY5jr1";
-// const PRIVATE_KEY = "your account private key";
-const PRIVATE_KEY = "b3e5b1136f7e6ad214a07566d8d8a4d4853ae7dd9e10c696b177b637f53a88fb";
+const PRIVATE_KEY = "your account private key";
 module.exports = {
   solidity: "0.8.10",
   networks: {
--- hh/scripts/edunation.js
+++ hh/scripts/edunation.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-    const ownerAddress = "0x575cA73E642983fF8818F0cb0Fa692A788Bc45A4"
+    const ownerAddress = "0xb95707D23Ba18d812a63275e16Ca0e2F05250533"
     const contract1 = await ethers.getContractFactory("Edunation");
     const edunation = await contract1.deploy(ownerAddress);
     console.log("edunation address : ", edunation.target);
